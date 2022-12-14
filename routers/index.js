fb78f7b025c7f3ca8d95d760b43c36fe97bbffd5
CuongNguyen <Cuongn1912@gamil.com>
2022-12-14 14:47:08 +0700

    feature checkout

--- routers/index.js
+++ routers/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { accountRouter } = require('./Account.router');
 const { categoryRouter } = require('./Category.router');
 const { productRouter } = require('./Product.router');
-// const { checkoutRouter } = require('./Checkout.router');
+const { checkoutRouter } = require('./Checkout.router');
 
 
 
@@ -10,7 +10,7 @@ const rootRouter = express.Router();
 rootRouter.use('/Account', accountRouter)
 rootRouter.use('/Category', categoryRouter)
 rootRouter.use('/Product', productRouter)
-// rootRouter.use('/Checkout', checkoutRouter)
+rootRouter.use('/Checkout', checkoutRouter)
 module.exports = {
     rootRouter
 }
