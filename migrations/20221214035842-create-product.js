12abc607a9d643b59dc6fdd7e1e0d651669addc7
CuongNguyen <Cuongn1912@gamil.com>
2022-12-14 13:01:51 +0700

    feature account

---
+++ migrations/20221214035842-create-product.js
@@ -0,0 +1,65 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ProductName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      ProductImage: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      Price: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      Discount: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      Material: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      Origin: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      Brand: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      Category_ID: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id'
+        }
+      },
+      IsActive: {
+        defaultValue: true,
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ routers/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { accountRouter } = require('./Account.router');
+// const { categoryRouter } = require('./Category.router');
+// const { productRouter } = require('./Product.router');
+// const { checkoutRouter } = require('./Checkout.router');
+
+
+
+const rootRouter = express.Router();
+rootRouter.use('/Account', accountRouter)
+// rootRouter.use('/Category', categoryRouter)
+// rootRouter.use('/Product', productRouter)
+// rootRouter.use('/Checkout', checkoutRouter)
+module.exports = {
+    rootRouter
+}
